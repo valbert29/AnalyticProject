43d7485a8701b2cdd3a605f2b77b797685b58b2c
Daryayan <daryasa1991@mail.ru>
2019-05-10 14:26:13 +0300

    Graphs application

--- AnaliticsFunctions/IParser.cs
+++ AnaliticsFunctions/IParser.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AnaliticsFunctions
+namespace Functions
 {
     public interface IMathFunc
     {
---
+++ MathFunction/DataVisualization/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataVisualization.MvcApplication" Language="C#" %>
