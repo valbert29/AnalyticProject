72c85c375ef93f064ea48f61620862d852423de0
Daryayan <daryasa1991@mail.ru>
2019-05-10 16:40:32 +0300

    function application

---
+++ AnaliticsFunctions/GraphScripts/Histogram.js
@@ -0,0 +1,19 @@
+﻿google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Dinosaur', 'Length'],
+        ['Acrocanthosaurus (top-spined lizard)', 12.2],
+        ['Albertosaurus (Alberta lizard)', 9.1],
+        ['Allosaurus (other lizard)', 12.2],
+        ['Apatosaurus (deceptive lizard)', 22.9]       
+    ]);
+
+    var options = {
+        title: 'Lengths of dinosaurs, in meters',
+        legend: { position: 'none' },
+    };
+
+    var chart = new google.visualization.Histogram(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
---
+++ AnaliticsFunctions/IParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functions
+{
+    public interface IMathFunc
+    {
+        double GetAverageValue(List<double> list);
+        double GetExpectedValue(List<double> list);
+        double GetDispersion(List<double> list);
+        double GetSquareDeviation(List<double> list);
+    }
+}
